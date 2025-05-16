d456e9f9ec46dd8b9669bc9c4378b1fdd6a7e7b2
benu <padhanbenu96@gmail.com>
2025-05-16 16:19:16 +0530

    Update files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/baatdekha/',
   plugins: [react()],
-  base: '/baatdekha',
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true  // Important for dev
+  }
+});
