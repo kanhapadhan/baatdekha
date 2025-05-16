025fdcce23a7927cbbbfc8aca5aacbbb65a3ee9e
benu <padhanbenu96@gmail.com>
2025-05-16 20:40:48 +0530

    firebase setup, bug fixes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/baatdekha/',
+  base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true  // Important for dev
-  }
 });
